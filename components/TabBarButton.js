26eacef2718a55e288601e431527fbbc571a5c15
Maxime Bertonnier <m.bertonnier@gmail.com>
2017-12-27 15:04:00 +0100

    Ajout de la prop onPress à TabBarButton

--- components/TabBarButton.js
+++ components/TabBarButton.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/Ionicons';
 import styles from '../styles/components/TabBarButton.style';
@@ -8,17 +8,22 @@ export default class TabBarButton extends Component {
 
     static propTypes = {
         icon: PropTypes.string.isRequired,
-        active: PropTypes.bool.isRequired
+        active: PropTypes.bool.isRequired,
+        onPress: PropTypes.func.isRequired
     }
 
     render () {
-        const { active, icon } = this.props;
+        const { active, icon, onPress } = this.props;
         const iconStyle = [styles.icon, active ? styles.activeIcon : {}];
 
         return (
-            <View style={styles.container}>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              style={styles.container}
+              onPress={onPress}
+            >
                 <Icon name={this.props.icon} style={iconStyle} />
-            </View>
+            </TouchableOpacity>
         );
     }
 }
--- styles/app.style.js
+++ styles/app.style.js
@@ -5,7 +5,7 @@ export default StyleSheet.create({
     container: {
         flex: 1
     },
-    listContainer: {
+    contentContainer: {
         flex: 1,
         backgroundColor: colors.darkBackground
     },
