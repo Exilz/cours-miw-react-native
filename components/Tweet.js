f0f5c5f77b849f8c8f18090319b4dc70f264ad25
Maxime Bertonnier <m.bertonnier@gmail.com>
2017-12-28 12:25:30 +0100

    Remplacer la donnée "en dur" par l'utilisation de props pour le composant Tweet

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
+import PropTypes from 'prop-types';
 import TweetButtons from './TweetButtons';
 import styles from '../styles/components/Tweet.style';
 
-const TWEET = {
-    id: 0,
-    user: 'Utilisateur',
-    nickname: 'Utilisateur',
-    date: '5m',
-    avatar: 'https://placeimg.com/150/150/animals',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida nulla vel justo iaculis, ut placerat erat mollis. Sed dapibus dolor eu nunc molestie, et imperdiet tortor lobortis. Nulla lorem neque, condimentum eget pulvinar ut, eleifend non nisi. Donec id lacus a risus scelerisque varius. Nunc gravida diam quis lorem tincidunt scelerisque',
-    images: ['https://i.imgur.com/dHLmxfOl.jpg', 'https://i.imgur.com/gSmWCJFl.jpg']
-};
-
 export default class Tweet extends Component {
+
+    static propTypes = {
+        id: PropTypes.number.isRequired,
+        user: PropTypes.string.isRequired,
+        nickname: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        avatar: PropTypes.string.isRequired,
+        content: PropTypes.string.isRequired,
+        images: PropTypes.array,
+    }
 
     get username () {
         return (
             <View style={styles.usernameContainer}>
-                <Text style={styles.username}>{ TWEET.nickname }</Text>
-                <Text style={styles.userAccount}>@{ TWEET.user }</Text>
-                <Text style={styles.tweetDate}>- { TWEET.date } </Text>
+                <Text style={styles.username}>{ this.props.nickname }</Text>
+                <Text style={styles.userAccount}>@{ this.props.user }</Text>
+                <Text style={styles.tweetDate}>- { this.props.date } </Text>
             </View>
         );
     }
@@ -28,19 +29,19 @@ export default class Tweet extends Component {
     get tweetContent () {
         return (
             <Text style={styles.tweet}>
-                { TWEET.content }
+                { this.props.content }
             </Text>
         );
     }
 
     get images () {
-        if (!TWEET.images || !TWEET.images.length) {
+        if (!this.props.images || !this.props.images.length) {
             return false;
         }
-        const images = TWEET.images.map((image, index) => {
+        const images = this.props.images.map((image, index) => {
             return (
                 <Image
-                  key={`${TWEET.id}-image-${index}`}
+                  key={`${this.props.id}-image-${index}`}
                   source={{ uri: image }}
                   style={styles.image}
                 />
@@ -57,7 +58,7 @@ export default class Tweet extends Component {
         return (
             <View style={styles.container}>
                 <View style={styles.avatarContainer}>
-                    <Image style={styles.avatar} source={{ uri: TWEET.avatar }} />
+                    <Image style={styles.avatar} source={{ uri: this.props.avatar }} />
                 </View>
                 <View style={styles.contentContainer}>
                     { this.username }
