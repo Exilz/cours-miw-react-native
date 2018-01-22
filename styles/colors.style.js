8644b44ce7bf2c84c29b1bc5f9db5f8f393e1c7e
Maxime Bertonnier <m.bertonnier@gmail.com>
2018-01-22 18:59:20 +0100

    Ajout de la couleur du menu

--- styles/colors.style.js
+++ styles/colors.style.js
@@ -1,6 +1,7 @@
 export default {
     lightBackground: '#243447',
     darkBackground: '#1b2836',
+    backgroundSecond: '#19242f',
     support: '#1da1f2',
     textPrimary: '#FFFFFF',
     textSecond: '#8899a6'
