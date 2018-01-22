0d07deaeb781cc6ee14717434be0d515df7cb0c8
Maxime Bertonnier <m.bertonnier@gmail.com>
2018-01-22 19:44:56 +0100

    Ajout d'une vue NewTweet d'exemple

---
+++ views/NewTweet.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class NewTweet extends Component {
+    render () {
+        return (
+            <Text>NEW TWEET</Text>
+        );
+    }
+}
