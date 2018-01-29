1999032aa22df3ee604e99eb900dbd4c22b225bc
Maxime Bertonnier <m.bertonnier@gmail.com>
2018-01-29 18:15:56 +0100

    Contenu de la vue NewTweet

---
+++ styles/views/NewTweet.style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import colors from '../colors.style';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.darkBackground
+    },
+    textInput: {
+        color: colors.textSecond
+    }
+})
--- views/NewTweet.js
+++ views/NewTweet.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import NewTweetBar from '../components/NewTweetBar';
+import styles from '../styles/views/NewTweet.style';
+import colors from '../styles/colors.style';
 
 export default class NewTweet extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = { text: '' };
+        this._onChangeText = this._onChangeText.bind(this);
+    }
+
+    _onChangeText (text) {
+        this.setState({ text });
+    }
+
     render () {
         return (
-            <Text>NEW TWEET</Text>
+            <View style={styles.container}>
+                <TextInput
+                  style={styles.textInput}
+                  onChangeText={this._onChangeText}
+                  underlineColorAndroid={colors.support}
+                />
+                <NewTweetBar text={this.state.text} />
+            </View>
         );
     }
 }
